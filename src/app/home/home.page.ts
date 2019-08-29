070b3eddf098c04f0d5be040dffe3995f14097fa
dem19 <dem19vieira@gmail.com>
2019-08-29 11:29:58 -0300

    Primeiro_commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  nome: string = "Demerson";
 
+ 
+  cor = "dark";
+
+  cores = ["danger","success","secondary","warning"]
+  idx = 0
+
+  humor = "sad";
+  
+  checked_humor = false;
+
+  constructor() { }
+
+  trocarCor(): void {
+    console.log('chamou trocaCor!');
+    if (this.idx > this.cores.length) {
+      this.idx = 0;
+    
+    } 
+    this.cor = this.cores[this.idx];
+    this.idx ++;
+    
+  }
+
+
+  mudaHumor(): void{
+    console.log('chamou muda o humor!');
+    this.checked_humor = !this.checked_humor;
+    if (this.checked_humor == true){
+      this.humor = "happy";
+    }else{
+      this.humor = "sad";
+    }
+  }
 }
+
+
+
